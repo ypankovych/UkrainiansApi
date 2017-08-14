4993e685757f101e8df3e0a7f5e73e524a360270
P-Alban <31005942+P-Alban@users.noreply.github.com>
2017-08-14 15:27:22 +0300

    Update and rename README.md to README.py

---
+++ README.py
@@ -0,0 +1,29 @@
+# Python UkrainiansApi
+
+## All methods:
+
+1. addComment
+2. deleteFriends
+3. acceptFollower
+4. getFollowers
+5. getMessages
+6. getDialogs
+7. getFriends
+8. sendMessage
+9. searchUsers
+10. inviteAllFriendsInGroup
+11. uploadImage
+12. makeLike
+13. makeShare
+14. wallPost
+
+# Examples for each method in method documentation.
+For example type:
+
+from UkrainiansApi import inviteAllFriendsInGroup
+print(inviteAllFriendsInGroup.__doc__)
+
+and you will se this:
+		
+		Example: inviteAllFriendsInGroup(groupID = 1337)
+		This method will added all your friends into the group
