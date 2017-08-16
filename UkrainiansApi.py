af3ca49f26155639546102cec73d2a9ab6ded55a
Pavel Durmanov <31005942+P-Alban@users.noreply.github.com>
2017-08-16 18:49:04 +0300

    Update UkrainiansApi.py

--- UkrainiansApi.py
+++ UkrainiansApi.py
@@ -1,31 +1,9 @@
 # coding: utf8
 __author__ = 'Alban'
 import requests
+import MethodsUrls
 
 class Session:
-
-	# Do not modify this urls!
-	userSearchUrl = 'https://api.ukrainians.co/v1/search?cityId={0}&countryId{1}0&gender={2}&isOnline={3}&maxAge={4}&minAge={5}&relationship={6}&schoolId={7}&skip={8}&top={9}&universityId={10}&value='
-	inviteUrl = 'https://api.ukrainians.co/v1/community/invite/{}'
-	inviteFriendsUrl = 'https://api.ukrainians.co/v1/community/inviteFriends/{}?skip={}'
-	imageUploadUrl = 'https://api.ukrainians.co/v1/post/uploadimages'
-	url = 'https://api.ukrainians.co/v1/token'
-	postUrl = 'https://api.ukrainians.co/v1/post/add'
-	likeURL = 'https://api.ukrainians.co/v1/like'
-	sendMsgUrl = 'https://api.ukrainians.co/v1/messenger/send'
-	getFriendsUrl = 'https://api.ukrainians.co/v1/contacts/friends?isCounts=false&isUser=true&online=false&routeId={}&skip={}'
-	getDialogsUrl = 'https://api.ukrainians.co/v1/dialog/get?skip=0'
-	getMessageUrl = 'https://api.ukrainians.co/v1/messenger/get?dialogId={}&skip=0'
-	getFollowersUrl = 'https://api.ukrainians.co/v1/contacts/followers?isUser=true&routeId={}&skip={}'
-	acceptFollowersUrl = 'https://api.ukrainians.co/v1/contacts/accept'
-	deleteFriendUrl = 'https://api.ukrainians.co/v1/contacts/delete'
-	commentAddUrl = 'https://api.ukrainians.co/v1/comment/add'
-	communitySearchUrl = 'https://api.ukrainians.co/v1/community/search?orderby={}&skip={}'
-	communityFollowUrl = 'https://api.ukrainians.co/v1/community/follow/{}'
-	unFollowUrl = 'https://api.ukrainians.co/v1/community/unfollow/{}'
-	getMyGroupsUrl = 'https://api.ukrainians.co/v1/community/my?includeCount=true'
-	getMutualFriendsUrl = 'https://api.ukrainians.co/v1/contacts/mutual?isUser=true&routeId={}&skip={}'
-
 	def __init__(self, userName, password):
 		# Authorization payloads
 		self.AuthPL = {
@@ -34,7 +12,7 @@ class Session:
 			'password': password
 		}
 		# Info about your account
-		self.usrinfo = requests.post(Session.url, data = self.AuthPL).json()
+		self.usrinfo = requests.post(MethodsUrls.url, data = self.AuthPL).json()
 		# Request headers
 		self.headers = {
 			'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
@@ -42,33 +20,49 @@ class Session:
 			'Authorization': 'bearer ' + self.usrinfo['access_token']
 		}
 
+	def setProfilePhoto(self, path, url = False):
+		'''
+		Example: setProfilePhoto(path = 'https://site/image.png/', url = True)
+		Params: set path to folder on your PC if param url = False.
+		If url = True, set url to image
+		'''
+		if url:
+			uploadImagePayLoads = [
+				('images', ('foo.png', requests.get(path).content, 'image/png'))
+			]
+		else:
+			uploadImagePayLoads = [
+		        ('images', ('foo.png', open(path, 'rb'), 'image/png'))
+			]
+		requests.post(MethodsUrls.setPhotoUrl, headers = self.headers, files = uploadImagePayLoads)
+
 	def getMutualFriends(self, userID, skip = 0):
 		'''
 		Example: getMutualFriends(userID = 1337, skip = 0)
 		This method will return json object
 		'''
-		return requests.get(Session.getMutualFriendsUrl.format(userID, skip), headers = self.headers).json()
+		return requests.get(MethodsUrls.getMutualFriendsUrl.format(userID, skip), headers = self.headers).json()
 
 	def getGroups(self):
 		'''
 		Example: getGroups()
 		This method will return json object
 		'''
-		return requests.get(Session.getMyGroupsUrl, headers = self.headers).json()
+		return requests.get(MethodsUrls.getMyGroupsUrl, headers = self.headers).json()
 
 	def leaveGroup(self, groupID):
 		'''
 		Example: leaveGroup(1337)
 		This method will return json object
 		'''
-		return requests.post(Session.unFollowUrl.format(groupID), headers = self.headers).json()
+		return requests.post(MethodsUrls.unFollowUrl.format(groupID), headers = self.headers).json()
 
 	def joinGroup(self, groupID):
 		'''
 		Example: joinGroup(groupID = 1337)
 		This method will return json object
 		'''
-		return requests.post(Session.communityFollowUrl.format(groupID), headers = self.headers).json()
+		return requests.post(MethodsUrls.communityFollowUrl.format(groupID), headers = self.headers).json()
 
 	def searchGroups(self, skip = 0, orderby = 0):
 		'''
@@ -77,14 +71,14 @@ class Session:
 		Param 'orderby', values: 0 - sort by relevance, 1 - sort by followers count
 		This method will return json object
 		'''
-		return requests.get(Session.communitySearchUrl.format(orderby, skip), headers = self.headers).json()
+		return requests.get(MethodsUrls.communitySearchUrl.format(orderby, skip), headers = self.headers).json()
 
 	def addComment(self, postID, text):
 		'''
 		Example: addComment(postID = 'post_id', text = 'Hello world')
 		This method will return json object
 		'''
-		return requests.post(Session.commentAddUrl, data = {
+		return requests.post(MethodsUrls.commentAddUrl, data = {
 			"contentType":0,
 			"contentId":postID,
 			"text":text}, headers = self.headers).json()
@@ -94,7 +88,7 @@ class Session:
 		Example: deleteFriends(userID = 1337)
 		This method will return json object
 		'''
-		return requests.post(Session.deleteFriendUrl, data = {
+		return requests.post(MethodsUrls.deleteFriendUrl, data = {
 			"routeId": userID
 			}, headers = self.headers).json()
 
@@ -103,7 +97,7 @@ class Session:
 		Example: acceptFollower(userID = 1337)
 		This method will return json object
 		'''
-		return requests.post(Session.acceptFollowersUrl, data = {
+		return requests.post(MethodsUrls.acceptFollowersUrl, data = {
 			"routeId": userID
 			}, headers = self.headers).json()
 
@@ -115,14 +109,14 @@ class Session:
 		'''
 		if userID is None:
 			userID = self.usrinfo['routeId']
-		return requests.get(Session.getFollowersUrl.format(userID, skip), headers = self.headers).json()
+		return requests.get(MethodsUrls.getFollowersUrl.format(userID, skip), headers = self.headers).json()
 
 	def getMessages(self, userID):
 		'''
 		Example: getMessages(userID = 1337)
 		This method will return json object
 		'''
-		return requests.get(Session.getMessageUrl.format(userID), headers = self.headers).json()
+		return requests.get(MethodsUrls.getMessageUrl.format(userID), headers = self.headers).json()
 
 	def getDialogs(self):
 		'''
@@ -130,7 +124,7 @@ class Session:
 		This method will return json object
 		This method will return 20 dialogs, for get more, set the skip param
 		'''
-		return requests.get(Session.getDialogsUrl, headers = self.headers).json()
+		return requests.get(MethodsUrls.getDialogsUrl, headers = self.headers).json()
 
 	def getFriends(self, userID = None, skip = 0): # return 20 friends without offset
 		'''
@@ -140,14 +134,14 @@ class Session:
 		'''
 		if userID is None:
 			userID = self.usrinfo['routeId']
-		return requests.get(Session.getFriendsUrl.format(userID, skip), headers = self.headers).json()
+		return requests.get(MethodsUrls.getFriendsUrl.format(userID, skip), headers = self.headers).json()
 
 	def sendMessage(self, ID, text):
 		'''
 		Example: sendMessage(userID = 1337, text = 'hello')
 		This method will return json object
 		'''
-		return requests.post(Session.sendMsgUrl, data = {"text":text,
+		return requests.post(MethodsUrls.sendMsgUrl, data = {"text":text,
 			"dialogId":ID}, headers = self.headers).json()
 
 	def searchUsers(self, sex = 0, city = 0, country = 0, online = 'false', relationship = 0, maxage = 100, minage = 0, skip = 0, school = 0, universityId = 0, top = 40):
@@ -156,7 +150,7 @@ class Session:
 		Example: searchUsers(sex = 0, city = 0, country = 0, online = 'false', relationship = 0, maxage = 100, minage = 0, skip = 0, school = 0, universityId = 0, top = 40)
 		This method will return json object
 		'''
-		return requests.get(Session.userSearchUrl.format(
+		return requests.get(MethodsUrls.userSearchUrl.format(
 			city, country, sex, online, maxage, minage, relationship, school, skip, top, universityId), headers = self.headers).json()
 
 	def inviteAllFriendsInGroup(self, groupID, skip = 0):
@@ -164,9 +158,9 @@ class Session:
 		Example: inviteAllFriendsInGroup(groupID = 1337, skip = 0)
 		This method will added all your friends into the group
 		'''
-		while requests.get(Session.inviteFriendsUrl.format(groupID, skip), headers = self.headers).json():
-			for User in requests.get(Session.inviteFriendsUrl.format(groupID, skip), headers = self.headers).json():
-				requests.post(Session.inviteUrl.format(groupID), data = {
+		while requests.get(MethodsUrls.inviteFriendsUrl.format(groupID, skip), headers = self.headers).json():
+			for User in requests.get(MethodsUrls.inviteFriendsUrl.format(groupID, skip), headers = self.headers).json():
+				requests.post(MethodsUrls.inviteUrl.format(groupID), data = {
 					"routeId": User['routeId']
 					}, headers = self.headers)
 
@@ -185,14 +179,14 @@ class Session:
 			uploadImagePayLoads = [
 		        ('images', ('foo.png', open(path, 'rb'), 'image/png'))
 			]
-		return requests.post(Session.imageUploadUrl, files = uploadImagePayLoads, headers = self.headers).json()
+		return requests.post(MethodsUrls.imageUploadUrl, files = uploadImagePayLoads, headers = self.headers).json()
 
 	def makeLike(self, postID):
 		'''
 		Example: makeLike(postID = 1337)
 		This method will return json object
 		'''
-		return requests.post(Session.likeURL, data = {
+		return requests.post(MethodsUrls.likeURL, data = {
 			"contentId": postID,
 			"likeType": 0,
 			"isLiked": 'false'
@@ -203,10 +197,10 @@ class Session:
 		Example: makeShare(postID = 1337, text = 'hello')
 		This method will return json object
 		'''
-		return requests.post(Session.postUrl, data = {
+		return requests.post(MethodsUrls.postUrl, data = {
 			"sharePostId":postID,
 			"text":text,
-			"receiverId":requests.post(Session.url, data = self.headers).json()['id'],
+			"receiverId":requests.post(MethodsUrls.url, data = self.headers).json()['id'],
 			"receiverType":0,
 			"ownerType":0
 			}, headers = self.headers).json() # Share
@@ -219,7 +213,7 @@ class Session:
 		if not ID:
 			ID = self.usrinfo['routeId']
 		receiverType = 0 if ID > 0 else 1
-		return requests.post(Session.postUrl, data = {
+		return requests.post(MethodsUrls.postUrl, data = {
 			"receiverId":ID,
 			"receiverType":receiverType,
 			"ownerType":receiverType,
